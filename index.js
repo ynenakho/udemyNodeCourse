4f6c8f73047c501121c01310259b8bcef0ea0b79
Yuriy Nenakhov <yuriy.nenakhov@gmail.com>
2019-01-29 18:59:56 -0800

    testing

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-const express = require('express');
+// const express = require('express');
+import express from 'express';
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
+const keys = require('./config/keys');
 
-app.get('/', (req, res) => {
-    res.send({bye: "WTF!!!"});
-});
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret
+}));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
