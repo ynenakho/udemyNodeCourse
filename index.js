eae753cde554bffd3a06e8a6b1a688056c0708c8
Yuriy Nenakhov <yuriy.nenakhov@gmail.com>
2019-01-30 20:00:06 -0800

    finished auth flow

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-// const express = require('express');
-import express from 'express';
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const app = express();
 const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
 
-passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret
-}));
+mongoose.connect(keys.mongoURI);
+
+const app = express();
+
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
